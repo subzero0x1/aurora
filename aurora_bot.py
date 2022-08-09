436f455556417e37b7991a79c13134cd9d01deda
Subzero <dima@svalov.ru>
2022-08-09 16:30:35 +0300

    hi on startup

--- aurora_bot.py
+++ aurora_bot.py
@@ -5,6 +5,7 @@ from aiogram import Bot, Dispatcher, executor, types
 # t.me/aurora_personal_assistant_bot
 
 API_TOKEN = ''
+USER_ID = 1
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -19,16 +20,21 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    if message.from_user.id != 1:
+    if message.from_user.id != USER_ID:
         return
     await message.reply("Hi!\nI'm Aurora, EchoBot!")
 
 
 @dp.message_handler()
 async def aurora_bot(message: types.Message):
-    if message.from_user.id != 1:
+    if message.from_user.id != USER_ID:
         return
     await message.answer(message.text)
 
+
+async def on_startup(_):
+    await bot.send_message(USER_ID, 'Hi!')
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
