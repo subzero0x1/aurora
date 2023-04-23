d199f0b2fd395941e169ff461d923dba8ea50bc8
Subzero <dima@svalov.ru>
2023-04-23 20:50:37 +0300

    quotes bot

--- aurora_bot.py
+++ aurora_bot.py
@@ -3,7 +3,6 @@ import logging
 import os
 import random
 
-# import aiocron as aiocron
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
@@ -13,8 +12,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import API_TOKEN
 from config import USER_ID
-
-GREETING = 'Bonjour!'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -32,7 +29,7 @@ class SaveText(StatesGroup):
 async def send_welcome(message: types.Message):
     if message.from_user.id != USER_ID:
         return
-    await message.reply(GREETING)
+    await on_startup()
 
 
 @dp.message_handler(Text(equals="Quote"))
@@ -62,23 +59,28 @@ async def save_text(message: types.Message, state: FSMContext):
     markup.add(button_save, button_skip)
 
     async with state.proxy() as data:
-        data["text"] = message.text
+        data["message"] = message
 
-    await message.reply(text=message.text, reply_markup=markup)
+    await message.reply(text='Your command?', reply_markup=markup)
     await SaveText.next()
 
 
 @dp.callback_query_handler(text=["remember", "ignore"], state=SaveText.waiting_for_confirmation)
 async def process_callback_save(callback_query: types.CallbackQuery, state: FSMContext):
+    user_id = callback_query.from_user.id
+    if user_id != USER_ID:
+        return
+    async with state.proxy() as data:
+        message = data["message"]
+        message_text = message.text
+        await message.delete()
     if callback_query.data == "remember":
-        async with state.proxy() as data:
-            text = data["text"]
-            user_id = callback_query.from_user.id
-            with open(f"{user_id}.txt", "a", encoding="utf-8") as f:
-                f.write(f"{text}\n")
-            await bot.answer_callback_query(callback_query.id, text="Copied that, Sir!")
+        with open(f"{user_id}.txt", "a", encoding="utf-8") as f:
+            f.write(f"{message_text}\n")
+        await bot.answer_callback_query(callback_query.id, text="Copied that, Sir!")
     else:
         await bot.answer_callback_query(callback_query.id, text="OK")
+    await callback_query.message.delete()
     await state.finish()
 
 
@@ -89,14 +91,11 @@ async def aurora_bot(message: types.Message):
     await message.answer(message.text)
 
 
-# aiocron.crontab('0 13 * * *', func=send_quote)
-
-
 async def on_startup():
     dp.register_message_handler(send_quote, commands=["quote"])
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(types.KeyboardButton("Quote"))
-    await bot.send_message(USER_ID, GREETING, reply_markup=markup)
+    await bot.send_message(USER_ID, 'Bonjour!', reply_markup=markup)
 
 
 if __name__ == '__main__':
